c91c7e873059329c18ef22be312814f015d4f059
Arjuna Anuhass <anuhass2017@gmail.com>
2025-08-29 12:18:33 +0530

    feat: navbar scroll disappear

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 
 function Header() {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setShow(false);
+      } else {
+        setShow(true);
+      }
+      setLastScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
 
   return (
-    <header className="fixed top-0 left-0 w-full z-30 bg-transparent text-primary font-light">
+    <header
+      className={`fixed top-0 left-0 w-full z-30 bg-transparent text-primary font-light transition-transform duration-300 ${
+        show ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
       <Navbar />
     </header>
   );

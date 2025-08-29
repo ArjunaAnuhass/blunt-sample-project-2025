e6c8a1bdb1caa61d723babdc79d084bb3c9bcc7a
Arjuna Anuhass <anuhass2017@gmail.com>
2025-08-29 16:05:19 +0530

    feat: our pillar section and swiper

--- src/components/common/BackgroundCommon.tsx
+++ src/components/common/BackgroundCommon.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ProductDescription from "../ProductDescription";
+import OurPillars from "../OurPillars";
 
 function Sustainability() {
+  
   return (
-    <div className="relative z-10 bg-white min-h-screen">
+    <div className="relative z-10 bg-white min-h-screen font-bold leading-1">
       <ProductDescription />
+      <OurPillars />
     </div>
   );
 }
